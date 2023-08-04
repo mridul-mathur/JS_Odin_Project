5497222304b034af8e59f263cad4efc61907667b
Mridulish_26 <mridulmathur2630@gmail.com>
2023-08-04 23:38:24 +0530

    js

--- RPS Game/rpscript.js
+++ RPS Game/rpscript.js
@@ -1,58 +1,9 @@
-const moves = ['rock', 'paper', 'scissors'];
-let playerScore = 0;
-let computerScore = 0;
-console.log('Welcome to Rock, Paper, Scissors');
-console.log('First to 5 wins');
-const winner = [
-    {
-        name: 'rock',
-        beats: 'scissors',
-    },
-    {
-        name: 'paper',
-        beats: 'rock',
-    },
-    {
-        name: 'scissors',
-        beats: 'paper',
-    },
-];
-
-function computermove() {
-    const compmove = moves[Math.floor(Math.random() * moves.length)];
-    console.log(compmove);
-    return compmove;
-}
-function playermove() {
-    const playermove = prompt('Choose your move: rock, paper or scissors');
-    console.log(playermove);
-    return playermove;
-}
-function playRound(playermove, compmove) {
-    if (playermove == compmove) {
-        console.log('Draw');
-    }
-    else if (winner.some(winner => winner.name == playermove && winner.beats == compmove)) {
-        console.log('Player wins');
-        playerScore++;
-    }
-    else {
-        console.log('Computer wins');
-        computerScore++;
-    }
-    console.log('Player score: ' + playerScore);
-    console.log('Computer score: ' + computerScore);
-}
-while (true) {
-    playRound(playermove(), computermove());
-    if (playerScore == 5 || computerScore == 5) {
-        if (playerScore == 5) {
-            console.log('Player wins the game');
-            break;
-        }
-        else {
-            console.log('Computer wins the game');
-            break;
-        }
-    }
-}
+const playerScore = 0;
+const computerScore = 0;
+const playerScore_span = document.getElementById("user");
+const computerScore_span = document.getElementById("comp");
+const scoreBoard_div = document.querySelector('.score');
+const compMove_div = document.querySelector('.compmove');
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissor_div = document.getElementById('s');
