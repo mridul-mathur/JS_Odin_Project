4261277513ae85f40229061489ebdacc724fd41a
Mridulish_26 <91665131+mridul-mathur@users.noreply.github.com>
2024-03-15 01:18:27 +0530

    odin

---
+++ Sketch/sketch.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    const size = 16;
+    const defaultColor = '#242424';
+    const canvas = document.getElementById('canvas');
+    const currColor = document.getElementById('color');
+    const colorBtn = document.getElementById('colorbtn');
+    const clearBtn = document.getElementById('clear');
+    const rainbowBtn = document.getElementById('rainbow');
+    const eraseBtn = document.getElementById('erase');
+    const sizeBtn = document.getElementById('sizebtn');
+}
